1991258a253073c288b95932b727e7e268416878
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-09-01 09:32:54 +0530

    extractedSringsAndMethods

--- app/src/main/java/com/example/sathasdroid/ViewHoliday.kt
+++ app/src/main/java/com/example/sathasdroid/ViewHoliday.kt
@@ -9,6 +9,7 @@ import androidx.annotation.RequiresApi
 import com.example.sathasdroid.Entity.Holiday
 
 class ViewHoliday : AppCompatActivity() {
+    // Declare UI elements
     private lateinit var holiday:Holiday
     private lateinit var tvViewType:TextView
     private lateinit var tvViewName:TextView
@@ -19,21 +20,44 @@ class ViewHoliday : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_holiday)
+        // Initialize UI elements
+        initializeUIElements()
 
+        // Set up the back button click listener
+        setUpBackButtonListener()
+
+        // Retrieve holiday object from the intent's extras
+        retrieveHolidayFromIntent()
+
+        // Display holiday information in TextViews
+        displayHolidayInfo()
+    }
+
+    // Initialize UI elements
+    private fun initializeUIElements() {
         tvViewType = findViewById(R.id.tvViewType)
         tvViewName = findViewById(R.id.tvViewName)
         tvViewDay = findViewById(R.id.tvViewDay)
         imgBackBtn = findViewById(R.id.imgBackBtn)
-
+    }
+    // Set up the back button click listener
+    private fun setUpBackButtonListener() {
         imgBackBtn.setOnClickListener {
-            finish()
+            finish() // Close the current activity and go back
         }
-
+    }
+    // Retrieve holiday object from the intent's extras
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun retrieveHolidayFromIntent() {
         val bundle = intent.extras
-        holiday = bundle?.getSerializable("holiday",Holiday::class.java)!!
+        holiday = bundle?.getSerializable("holiday", Holiday::class.java)!!
+    }
 
+    // Display holiday information in TextViews
+    private fun displayHolidayInfo() {
         tvViewType.text = holiday.type
         tvViewDay.text = holiday.date
         tvViewName.text = holiday.name
     }
+
 }
